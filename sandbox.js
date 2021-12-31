94488423c22a6d1ce72c07fc959c46ef561d6572
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-31 12:01:20 +0100

    DOM & type casting

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,9 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,11 @@
-const character = 'mario'
+const form = document.querySelector('.new-item-form') as HTMLFormElement
 
-console.log(character)
+const type = document.querySelector('#type') as HTMLFormElement
+const toFrom = document.querySelector('#tofrom') as HTMLSelectElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
 
-const inputs = document.querySelectorAll('input')
-
-inputs.forEach((input) => {
-  console.log(input)
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber)
 })
